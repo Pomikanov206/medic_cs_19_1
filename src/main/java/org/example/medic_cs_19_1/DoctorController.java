866aec0fc20f78915bc1829da248ef4a4ac358b7
Pomikanov206 <pomikanov@gmail.com>
2022-09-26 10:14:01 +0300

    Create basic example
    Create doctor controller, patient template

---
+++ src/main/java/org/example/medic_cs_19_1/DoctorController.java
@@ -0,0 +1,14 @@
+package org.example.medic_cs_19_1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DoctorController {
+    @GetMapping("/patient")
+    public String showPatient(Model model) {
+        model.addAttribute("name", "Помиканов Андрій");
+        return "patient";
+    }
+}
---
+++ src/main/java/org/example/medic_cs_19_1/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package org.example.medic_cs_19_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
