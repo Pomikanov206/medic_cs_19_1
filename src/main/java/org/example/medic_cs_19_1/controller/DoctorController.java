38f3df4157a8b795293296aa398ef86b9e9cb1d1
Pomikanov206 <pomikanov@gmail.com>
2022-09-26 10:25:28 +0300

    Add models

--- src/main/java/org/example/medic_cs_19_1/controller/DoctorController.java
+++ src/main/java/org/example/medic_cs_19_1/controller/DoctorController.java
@@ -1,4 +1,4 @@
-package org.example.medic_cs_19_1;
+package org.example.medic_cs_19_1.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
---
+++ src/main/java/org/example/medic_cs_19_1/model/Patient.java
@@ -0,0 +1,45 @@
+package org.example.medic_cs_19_1.model;
+
+import java.util.List;
+
+public class Patient {
+    private String name;
+    private String phone;
+    private String address;
+    private List<Disease> diseases;
+
+    public Patient() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Disease> getDiseases() {
+        return diseases;
+    }
+
+    public void setDiseases(List<Disease> diseases) {
+        this.diseases = diseases;
+    }
+}
